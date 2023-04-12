44770581ba235fc14ad9b03e6f7afb9953744ca9
SorenKazam <tiago.guerreiro8957@gmail.com>
2023-04-12 21:08:55 +0100

    fixes

--- av.py
+++ av.py
@@ -1,16 +1,18 @@
-import requests
 import os
 import json
+import requests
 
-with open('list.json') as f:
-    data = json.load(f)
+url = 'https://raw.githubusercontent.com/SorenKazam/NOSAV/main/list.json'
+response = requests.get(url)
+data = json.loads(response.text)
 
 files = []
 cleanFiles = 0
 infectedFiles = 0
 totalFiles = 0
 
-for file in os.listdir():
+for file in os.listdir("folder/"):  # aqui é a pasta que ira ser analisado
+    print(f"Examinando ficheiro: {file}")
     if file == "av.py":
         continue
     files.append(file)
@@ -27,14 +29,6 @@ for file in files:
         print(f"O arquivo {file} não existe na lista JSON.")
         cleanFiles += 1
         totalFiles += 1
-
-
-url = 'https://raw.githubusercontent.com/<username>/<reponame>/<branch>/<filepath>.json'
-response = requests.get(url)
-data = json.loads(response.text)
-
-# Agora você pode acessar os dados do arquivo JSON como um objeto Python
-print(data['key'])
 
 print(
     f"{totalFiles} Ficheiros analisados, existem {cleanFiles} ficheiros limpos e {infectedFiles} ficheiros infetados!")
